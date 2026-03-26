73316ce92ad71b1a6a0067b47382b4601bbbe33f
laran <quintanarijoan@gmail.com>
2026-03-26 16:39:42 -0300

    feat: crea ruta y vista (sobre-mi)

---
+++ resources/views/sobre-mi.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre mí</title>
+     
+    <style> 
+        body { 
+        font-family: Arial; 
+        background-color: #f4f4f4; 
+        margin: 40px; 
+        } 
+        
+        .container { 
+        background: white; 
+        padding: 20px; 
+        border-radius: 10px; 
+        max-width: 600px; 
+        } 
+        
+        h1 { 
+        color: #333; 
+        } 
+        
+        p { 
+        line-height: 1.6; 
+        } 
+    </style>
+</head>
+<body>
+     
+    <h1>Sobre mí</h1> 
+    
+    <p><b>Nombre:</b> Antonio Quintana</p> 
+    
+    <p><b>Edad:</b> 26 años</p> 
+    
+    <p><b>De dónde soy:</b> Corrientes, Argentina</p> 
+    
+    <p><b>Me gustaría trabajar en:</b> Desarrollo de software</p> 
+    
+    <p><b>Expectativas del curso:</b> Aprender a crear aplicaciones 
+    web</p> 
+    
+    <p><b>Hobbies:</b> Programar, pool, pagode</p> 
+    
+</div>
+</body>
+</html>
