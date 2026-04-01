74e270c8e93e8ab69be85abe51da79889033c4e0
laran <quintanarijoan@gmail.com>
2026-04-01 18:13:43 -0300

    feat: botones cv y contactar

--- resources/views/sobre-mi.blade.php
+++ resources/views/sobre-mi.blade.php
@@ -17,23 +17,25 @@
             </div>
         </div>
     </nav>
+    <div class="container mt-4">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="card-title">Sobre mí</h1>
+                <p><b>Nombre:</b> Antonio Quintana</p> 
     
-    <h1>Sobre mí</h1> 
-    
-    <p><b>Nombre:</b> Antonio Quintana</p> 
-    
-    <p><b>Edad:</b> 26 años</p> 
-    
-    <p><b>De dónde soy:</b> Corrientes, Argentina</p> 
-    
-    <p><b>Me gustaría trabajar en:</b> Desarrollo de software</p> 
-    
-    <p><b>Expectativas del curso:</b> Aprender a crear aplicaciones 
-    web</p> 
-    
-    <p><b>Hobbies:</b> Programar, pool, pagode</p> 
-    
+                <p><b>Edad:</b> 26 años</p> 
+                
+                <p><b>De dónde soy:</b> Corrientes, Argentina</p> 
+                
+                <p><b>Me gustaría trabajar en:</b> Desarrollo de software</p> 
+                
+                <p><b>Expectativas del curso:</b> Aprender a crear aplicaciones 
+                web</p> 
+            </div>
+        </div>
     </div>
+    <a href="#" class="btn btn-primary mt-3">Descargar CV</a>
+    <a href="#" class="btn btn-secondary mt-3">Conectar</a>
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
