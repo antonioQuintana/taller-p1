6e01b34c288af22d30ba8bbf0c2794304fada536
laran <quintanarijoan@gmail.com>
2026-04-06 16:41:23 -0300

    feat: vista exito

---
+++ resources/views/exito.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP-4</title>
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css')}}">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="#" class="navbar-brand">Ejercicio TP-4</a>
+
+            <div class="navbar-nav">
+                <a href="/" class="nav-link">Inicio</a>
+                <a href="/sobre-mi" class="nav-link">Sobre mí</a>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-4">
+       
+            
+            <div class="alert alert-success" role="alert">
+            Recibimos tu mensaje. Un asesor comercial se comunicará contigo a la 
+brevedad. ¡Muchas gracias!
+            </div>
+            
+        
+    </div>
+    
+    
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+</body>
+</html>
